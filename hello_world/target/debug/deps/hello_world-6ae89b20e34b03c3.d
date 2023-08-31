1d5c042569f62a86e09371ab8030f7ff7c5777ca
Amadeusz Szewczyk <firma@MacBook-Air-2.local>
2023-08-31 21:47:23 +0200

    hello

---
+++ hello_world/target/debug/deps/hello_world-6ae89b20e34b03c3.d
@@ -0,0 +1,5 @@
+/Users/firma/projects/rustHello/hello_world/target/debug/deps/hello_world-6ae89b20e34b03c3: src/main.rs
+
+/Users/firma/projects/rustHello/hello_world/target/debug/deps/hello_world-6ae89b20e34b03c3.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/firma/projects/rustHello/hello_world/target/debug/hello_world: /Users/firma/projects/rustHello/hello_world/src/main.rs
